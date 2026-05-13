8415ff6dfc2d301c7a67fa9eb487a843b70d762b
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-05-13 20:12:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1586,7 +1586,7 @@ elif mode == "Textové předpovědi":
     forecast_placeholder = st.empty()
 
     # ---------------- OUTPUT ----------------
-    with st.spinner("Načítám data..."):
+    with st.spinner("Načítám předpověď..."):
 
         if active is None:
             forecast_placeholder.markdown(
